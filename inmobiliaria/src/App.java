39a2b6afab157a72d66fd030f98379ad77b547fc
basti <basti.mejiasss@gmail.com>
2024-08-26 21:36:26 -0400

    modularidad de funciones desde app a la clase "MENU"

--- inmobiliaria/src/App.java
+++ inmobiliaria/src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.mostrarMenu();
+        PropiedadCSV propiedadCSV = new PropiedadCSV();
+        MenuVista menuVista = new MenuVista();
+        MenuControl menuController = new MenuControl(propiedadCSV, menuVista);
+        menuController.iniciar();
     }
 }
